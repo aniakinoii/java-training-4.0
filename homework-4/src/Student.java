e99e870444292fb587e9fbb9b3dd44f16b5b8c57
Darya <aniakinoii@gmail.com>
2024-10-20 23:56:11 +0300

    merged own Student class with example

--- homework-4/src/Student.java
+++ homework-4/src/Student.java
@@ -1,9 +1,24 @@
 public class Student {
+    int rating;
     private String name;
-    private int rating;
-    public String getName(){
+
+    public Student() {
+        name = "";
+        rating = 0;
+    }
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public static double getAvgRating() {
+        // TODO return average rating of all students
+        return 0;
+    }
+
+    public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -14,27 +29,21 @@ public class Student {
 
     public void setRating(int rating) {
         this.rating = rating;
-    }
-    public Student() {
-        name = "";
-        rating = 0;
-    }
-    public Student(String name, int rating){
-        this.name = name;
-        this.rating = rating;
-    }
-//method to define the better student (between two, return true or false)
-// returns true when sa is better than sb
-    public static boolean betterStudent(Student sa, Student sb) {
-        return sa.rating > sb.rating;
-    }
-//method to output information about student
 
+    }
+
+    public boolean betterStudent(Student student) {
+        return this.rating > student.rating;
+    }
+
+    public void changeRating(int rating) {
+        // TODO change this student's rating and average rating of all students
+
+    }
+    
+
+    @Override
     public String toString() {
         return name + " rating: " + rating;
-    }
-
-    public void changeRating(int newRating){
-        rating = newRating;
     }
 }
