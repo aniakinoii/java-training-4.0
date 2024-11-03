d22ba42233e5418ad90fb5556be48fd7522d87bb
Darya <aniakinoii@gmail.com>
2024-11-03 23:19:53 +0200

    homework 5 java-basics-oop-p2 completed

--- src/Employee.java
+++ src/Employee.java
@@ -1,36 +1,38 @@
-public class Employee {
+public abstract class Employee {
     // TODO fix class declaration and declare variables here
+    private String employeeId;
+    private String name;
 
     public Employee(String employeeId, String name) {
-        // TODO fill in code here
+        this.employeeId = employeeId;
+        this.name = name;
     }
 
     public String getEmployeeId() {
-        // TODO fill in code here and replace the return statement
-        return "";
+        return employeeId;
     }
 
     public void setEmployeeId(String employeeId) {
-        // TODO fill in code here
+        this.employeeId = employeeId;
     }
 
     public String getName() {
-        // TODO fill in code here and replace the return statement
-        return "";
+        return name;
     }
 
     public void setName(String name) {
-        // TODO fill in code here
+        this.name = name;
     }
 
     public double getAverageMonthlySalary() {
-        // TODO fill in code here and replace the return statement
-        return 0;
+        return calculatePay();
     }
 
     @Override
     public String toString() {
-        // TODO fill in code here and replace the return statement, be sure to format double value
-        return "";
+        return String.format("name: %s employeeId: %s pay: %.2f",
+                name, employeeId, getAverageMonthlySalary());
     }
+
+    public abstract double calculatePay();
 }
