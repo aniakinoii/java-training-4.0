439b7e7ee74d57d15188901ecb3adbdc4336c5ab
Darya <aniakinoii@gmail.com>
2024-11-10 22:20:15 +0200

    homework-6

--- src/Zoo.java
+++ src/Zoo.java
@@ -7,10 +7,11 @@ public class Zoo {
      *
      * @param list list of birds of particular type.
      */
-    public static void feed(List list) {
-        // TODO fix method declaration
+    public static <T extends Bird> void feed(List<T> list) {
         System.out.println("Feeding birds");
-        // TODO feed birds in the cycle
+        for (var bird : list){
+            bird.eat();
+        }
     }
 
     /**
@@ -19,11 +20,13 @@ public class Zoo {
      * @param list list of birds of particular type.
      * @param bird a new bird.
      */
-    public static void acceptBird(List list, FlyingBird bird) {
-        // TODO fix method declaration
+    public static void acceptBird(List<? super FlyingBird> list, FlyingBird bird) {
         System.out.println("Accepting a bird to a section");
         // TODO check flying bird wings
+        bird.checkWings();
         // TODO add a bird to the list and print it
+        list.add(bird);
+        System.out.println(bird);
     }
 
     /**
@@ -32,10 +35,11 @@ public class Zoo {
      * @param list    list of birds.
      * @param newBird bird to add.
      */
-    public static void registerBird(List list, Bird newBird) {
-        // TODO fix method declaration
+    public static void registerBird(List<Bird> list, Bird newBird) {
         System.out.println("Adding a bird to the birds list");
-        // TODO add a bird to list
-        // TODO print birds in the cycle
+        list.add(newBird);
+        for (var bird : list){
+            System.out.println(bird);
+        }
     }
 }
