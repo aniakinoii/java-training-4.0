9b71e116ed359b9edb714406542139933bd61105
Darya <aniakinoii@gmail.com>
2024-12-26 18:00:07 +0200

    task completion

--- src/Temperature.java
+++ src/Temperature.java
@@ -7,8 +7,7 @@ public class Temperature {
      * @return temperature degree in Celsius
      */
     public static double fahrenheitToCelsius(double fahrenheit) {
-        // TODO fill in code here and replace the return statement
-        return 0;
+        return (5.0 * (fahrenheit - 32.0) / 9.0);
     }
 
     /**
@@ -18,7 +17,6 @@ public class Temperature {
      * @return temperature degree in Fahrenheit
      */
     public static double celsiusToFahrenheit(double celsius) {
-        // TODO fill in code here and replace the return statement
-        return 0;
+        return (celsius * 9 / 5) + 32;
     }
 }
--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,8 +14,10 @@ public class TemperatureTest {
         return new Object[][]{
                 {212.0, 100.0},
                 {32.0, 0.0},
-                {-15, -26.11111111111111}
-                // TODO add 2 more test data here
+                {-15, -26.11111111111111},
+                {455, 235},
+                {92, 33.333333333333336}
+
         };
     }
 
@@ -28,8 +30,9 @@ public class TemperatureTest {
         return new Object[][]{
                 {100.0, 212.0},
                 {0.0, 32.0},
-                {-26.11111111111111, -15}
-                // TODO add 2 more test data here
+                {-26.11111111111111, -15},
+                {235, 455},
+                {33.333333333333336, 92}
         };
     }
 
